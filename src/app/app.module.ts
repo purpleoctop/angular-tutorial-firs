9f2e18da9d4bd4d1aa77c612dbe2c838c37bc9aa
purpleoctop <marabidze@sdsu.edu>
2019-07-06 12:13:36 +0400

    feat(3-Managing-Data): Finish Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
@@ -18,15 +18,15 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
- ],
-
+    CartComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
       {path: 'products/:productId', component: ProductDetailsComponent},
-
+      {path: 'cart', component: CartComponent}
 
     ])
   ],
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
-
+import { CartService } from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -11,7 +11,7 @@ export class ProductDetailsComponent implements OnInit {
   product;
   constructor(
     private route: ActivatedRoute,
-
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
@@ -20,5 +20,9 @@ export class ProductDetailsComponent implements OnInit {
     ));
   }
 
+  addToCart(product) {
+    window.alert(" Your product has been added to the cart!");
+    this.cartService.addToCart(product);
+  }
 
 }
