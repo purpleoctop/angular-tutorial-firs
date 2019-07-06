bb5a6e2ac93ee97274a6351197ca4601b09f69e4
purpleoctop <marabidze@sdsu.edu>
2019-07-06 12:19:29 +0400

    feat(4-Shipping-Prices): Finish Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Router } from '@angular/router';
 import { NgModule } from '@angular/core';
-
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -18,16 +18,18 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     RouterModule.forRoot([
       {path: '', component: ProductListComponent},
       {path: 'products/:productId', component: ProductDetailsComponent},
-      {path: 'cart', component: CartComponent}
-
+      {path: 'cart', component: CartComponent},
+      {path: 'shipping', component: ShippingComponent}
     ])
   ],
   providers: [],
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
     providedIn: 'root'
@@ -7,7 +8,9 @@ import { Injectable } from '@angular/core';
 export class CartService {
     items = [];
 
-    constructor() { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
     addToCart(product) {
         this.items.push(product);
@@ -21,4 +24,10 @@ export class CartService {
         this.items = [];
         return this.items;
     }
+
+    getShippingPrices() {
+    return this.http.get('./assets/shipping.json');
+
+    }
+
 }
