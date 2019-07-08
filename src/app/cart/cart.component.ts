4d7aa1d1a180fe9667c65b40402c15096bca6e46
Ciyvi1998 <kakauridze.nika@gtu.ge>
2019-07-08 18:54:32 +0400

    feat(8-reaqtive-forms-nested-forms): finish tutorial

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, TestabilityRegistry } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CartService } from '../cart.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -17,8 +17,13 @@ export class CartComponent implements OnInit {
     this.items = this.cartService.getItems();
 
     this.checkoutForm = this.formBuilder.group({
-      name: '',
-      address: ''
+      name: ['', [Validators.minLength(2)]],
+      address: this.formBuilder.group({
+        street: '',
+        city: '',
+        state: '',
+        zip: ''
+    })
     });
   }
 
@@ -29,11 +34,8 @@ export class CartComponent implements OnInit {
   }
 
   setDefault() {
-    // this.checkoutForm.get('name').setValue('Jon Doe');
-    // this.checkoutForm.get('address').setValue('Tbilisi');
     this.checkoutForm.patchValue({
-      name: 'Jon Doe',
-      address: 'Tbilisi'
+      name: 'Jon Doe'
     });
   }
 
