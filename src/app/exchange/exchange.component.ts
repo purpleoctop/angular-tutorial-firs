79ddd987bcae30c0cf9b6a58d71f5dcd690f40de
Ciyvi1998 <kakauridze.nika@gtu.ge>
2019-07-10 21:07:45 +0400

    feat(Observable-tasks): Finish task-1 and task-2

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -9,8 +9,10 @@ import { ValueConverter } from '@angular/compiler/src/render3/view/template';
   styleUrls: ['./exchange.component.scss']
 })
 export class ExchangeComponent implements OnInit {
-  curr1;
-  curr2;
+  currencies = {
+    curr1: {rate: 0, symbol: "USD"},
+    curr2: {rate: 0, symbol: "USD"}
+  }
   data;
   to;
   constructor(private httpClient: HttpClient) {
@@ -18,23 +20,36 @@ export class ExchangeComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.getRate(this.curr1, 'EUR');
   }
 
 
-  getRate(currency1, currency2) {
-      const url = `https://api.exchangeratesapi.io/latest?base=${currency1}&symbols=${currency2}`;
-      this.httpClient
-        .get(url).subscribe(value => {
-         console.log(value['rates'][currency2]);
-        });
-}
-setCurrency1(curr) {
-  this.curr1 = curr;
-}
-setCurrency2(curr) {
-  this.curr2 = curr;
-}
+  getRate(currency) {
+    const currency2Symbol = currency === this.currencies.curr1 ? this.currencies.curr2 : this.currencies.curr1;
+    const url = `https://api.exchangeratesapi.io/latest?base=${currency.symbol}&symbols=${currency2Symbol.symbol}`;
+    this.httpClient
+      .get(url)
+      .subscribe(value => {
+        const currency2Key = Object.keys(this.currencies).find(k=> this.currencies[k].symbol === currency2Symbol.symbol)
+        console.log(currency, value )
+        this.currencies[currency2Key].rate = value['rates'][currency2Symbol.symbol] * currency.rate
+      });
+  }
+
+  setRate1(rate) {
+    this.currencies.curr1.rate = rate;
+    this.getRate(this.currencies.curr1);
+  }
+  setRate2(rate) {
+    this.currencies.curr2.rate = rate;
+    this.getRate(this.currencies.curr2);
+
+  }
+  setSymbol1(symbol) {
+    this.currencies.curr1.symbol = symbol;
+  }
+  setSymbol2(symbol) {
+    this.currencies.curr2.symbol = symbol;
+  }
 
 
 }
