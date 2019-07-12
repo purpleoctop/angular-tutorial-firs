9e4299341ebd16b63f8601d62587cd0c84b66942
purpleoctop <marabidze@sdsu.edu>
2019-07-12 12:57:00 +0400

    feat(Routing): Finish task-2

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@angular/core';
+import {LoginService } from './login.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-users;
-  constructor() { }
+  constructor(
+    private loginService : LoginService
+  ) { }
 
-
+logout(){
+  this.loginService.allowed=false;
+}
 
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Users } from '../users'
+import { UsersService } from "../users.service"
 
 @Component({
   selector: 'app-users',
@@ -8,10 +9,16 @@ import { Users } from '../users'
 })
 export class UsersComponent implements OnInit {
 users=[];
-  constructor() { }
+  constructor(
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
     this.users=Users;
+  }
+
+  log_out(){
+    return this.usersService.logout();
   }
 
 }
